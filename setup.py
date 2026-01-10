31833b63c8c763e1227050f51189086532e18273
gokula krishnan <gokulakrishnan1355@gmail.com>
2026-01-10 13:18:22 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medibuddy",
+    version="0.1",
+    author="Gokula Krishnan",
+    author_email='gokulakrishnan1355@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
